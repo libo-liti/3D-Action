b9803e01fa1501d1b58f7622f387e3b8c5b2b221
ZeroDimen <rlaehdrjs951@naver.com>
2026-01-06 14:36:10 +0900

    Add Intro Scene
    2026.01.06
    이름을 작성할 Intro Scene 추가 및 주석 작성

--- Assets/02. Script/Photon/Simple_NetworkManager.cs
+++ Assets/02. Script/Photon/Simple_NetworkManager.cs
@@ -1,12 +1,17 @@
-using System;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Simple_NetworkManager : MonoBehaviourPunCallbacks
 {
     private string gameVersion = "1";
 
+    [SerializeField] private TMP_InputField nickNameField;
+    [SerializeField] private Button connectButton;
+    
     private void Awake()
     {
         Screen.SetResolution(1920, 1080, false); // 해상도 설정
@@ -17,24 +22,26 @@ public class Simple_NetworkManager : MonoBehaviourPunCallbacks
 
     private void Start()
     {
-        Connect();
+        connectButton.onClick.AddListener(Connect);
     }
 
-    private void Connect()
+    private void Connect() // Photon Master Server에 접속을 요청하는 함수
     {
-        PhotonNetwork.ConnectUsingSettings();
-        Debug.Log("서버 접속");
+        PhotonNetwork.NickName = nickNameField.text;
+        
+        PhotonNetwork.ConnectUsingSettings(); 
+        Debug.Log("Master Server 접속");
     }
 
-    public override void OnConnectedToMaster()
+    public override void OnConnectedToMaster() // 방 생성 및 생성 되었다면 접속하는 함수 (Master Server에 접속시 호출됨)
     {
-        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 20 }, null);
-        Debug.Log("서버 접속 완료");
+        PhotonNetwork.JoinOrCreateRoom("MMORPG_Room", new RoomOptions { MaxPlayers = 5 }, null);
+        Debug.Log("MMORPG_Room 접속 완료");
     }
 
-    public override void OnJoinedRoom()
+    public override void OnJoinedRoom() // 캐릭터를 생성하는 함수 (방 접속 완료시 호출됨)
     {
-        PhotonNetwork.Instantiate("Player", Vector3.up, Quaternion.identity);
-        Debug.Log("캐릭터 생성");
+        PhotonNetwork.LoadLevel("Main");
+        Debug.Log("씬 전환");
     }
 }
